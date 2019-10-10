4fe8a63b6744ed93b642109bb88b02fa512f7295
MattR246 <49105348+MattR246@users.noreply.github.com>
2019-10-10 08:14:16 -0500

    moved directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -32,4 +32,7 @@ const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))
 });
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(
+    null, 
+    mapDispatchToProps
+)(CollectionItem);
